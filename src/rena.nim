e2c80204918b420bc08fee1550aa4fd041041237
jiro4989 <jiroron666@gmail.com>
2020-03-27 01:02:12 +0900

    version up [skip ci]

--- src/rena.nim
+++ src/rena.nim
@@ -3,7 +3,7 @@ from strformat import `&`
 
 const
   whiteSpaces = @[" ", "　", "\t"]
-  version = """renames version 1.0.0
+  version = """renames version 1.0.1
 Copyright (c) 2019 jiro4989
 Released under the MIT License.
 https://github.com/jiro4989/rena"""
